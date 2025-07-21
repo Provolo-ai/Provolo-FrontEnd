eee4428b7826fa7b84f47f955c34612f2a35330d
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-07-21 22:14:11 +0100

    feat: implement user signup functionality with email and password

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,5 +31,5 @@ if (typeof window !== "undefined") {
 
 // Export initialized services
 export const auth = getAuth(app);
-export const db = getDatabase(app); // or getFirestore(app)
+export const db = getFirestore(app);
 export { app };
